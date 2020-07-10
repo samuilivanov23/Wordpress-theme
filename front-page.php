773748e39146ae4e383c1b34ec4c2005a8e9c909
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-07-09 23:29:52 -0700

    loading the map api script in the head

--- front-page.php
+++ front-page.php
@@ -3,7 +3,6 @@
     <h1> The map with locations</h1>
 
     <div id="map">
-        <p>not working.... should be royaleblue</p>
     </div>
 
 <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
         wp_enqueue_script('customjs');
 
         $map_api_key = "AIzaSyAx04NtoyYgnm6kBsk7JTi6T4GAYZ-Ar28";
-        wp_enqueue_script('googleapis', esc_url( add_query_arg( 'key', $map_api_key.'&callback=initMap', '//maps.googleapis.com/maps/api/js' )), array(), null, true );
+        wp_enqueue_script('googleapis', esc_url( add_query_arg( 'key', $map_api_key.'&callback=initMap', '//maps.googleapis.com/maps/api/js' )), array(), null, false );
     }
     add_action('wp_enqueue_scripts', 'load_javascript');
 
@@ -21,7 +21,7 @@
     function add_async_defer_attribute($tag, $handle) {
         if ( 'googleapis' !== $handle )
         return $tag;
-        return str_replace( ' src', ' async defer src', $tag );
+        return str_replace( 'src', 'async defer src', $tag );
     }
     add_filter('script_loader_tag', 'add_async_defer_attribute', 10, 2);
 ?>

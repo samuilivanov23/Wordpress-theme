a898da5260dd806746c6e493b2165fd0e53fb8ca
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-07-12 23:44:07 -0700

    Adding script for loading data into the locations table

---
+++ php-mysql-scripts/load-data-into-locations.php
@@ -0,0 +1,80 @@
+<?php
+    //getting the dataset form the opendatasoft api.
+    $cURLConnection = curl_init();
+
+    curl_setopt($cURLConnection, CURLOPT_URL, 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=&rows=5000&start=1');
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+    $locations = curl_exec($cURLConnection);
+
+    curl_close($cURLConnection);
+
+    //database credentials
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "wordpressadmin";
+    $dbname = "wordpress";
+
+    //connecting to the database
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    
+    //checking if the connection is successful
+    if($connection -> connect_error)
+    {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $jsonArrayResponse = json_decode($locations, true);
+    //echo count($jsonArrayResponse["records"]) . " test: \n";
+
+    for ($i = 0; $i < count($jsonArrayResponse["records"]); $i++) 
+    {
+        $zip_code = $jsonArrayResponse["records"][$i]["fields"]["zip"];
+        $city = $jsonArrayResponse["records"][$i]["fields"]["city"];
+        $state = $jsonArrayResponse["records"][$i]["fields"]["state"];
+        $latitude = $jsonArrayResponse["records"][$i]["fields"]["latitude"];
+        $longitude = $jsonArrayResponse["records"][$i]["fields"]["longitude"];
+
+        $sql = "INSERT INTO locations(zipcode, city, state_, latitude, longitude)
+            VALUES ('".$zip_code."', '".$city."', '".$state."', '".$latitude."', '".$longitude."')";
+        if(!mysqli_query($connection, $sql))
+        {
+           die('Error while executing sql query');
+        }
+    }
+
+    //getting the dataset form the opendatasoft api.
+    $cURLConnection = curl_init();
+
+    curl_setopt($cURLConnection, CURLOPT_URL, 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=&rows=3999&start=6000');
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+    $second_locations = curl_exec($cURLConnection);
+
+    curl_close($cURLConnection);
+
+
+    $jsonArrayResponse = json_decode($second_locations, true);
+    //echo count($jsonArrayResponse["records"]) . " test: \n";
+
+    for ($i = 0; $i < count($jsonArrayResponse["records"]); $i++) 
+    {
+        $zip_code = $jsonArrayResponse["records"][$i]["fields"]["zip"];
+        $city = $jsonArrayResponse["records"][$i]["fields"]["city"];
+        $state = $jsonArrayResponse["records"][$i]["fields"]["state"];
+        $latitude = $jsonArrayResponse["records"][$i]["fields"]["latitude"];
+        $longitude = $jsonArrayResponse["records"][$i]["fields"]["longitude"];
+
+        $sql = "INSERT INTO locations(zipcode, city, state_, latitude, longitude)
+            VALUES ('".$zip_code."', '".$city."', '".$state."', '".$latitude."', '".$longitude."')";
+        if(!mysqli_query($connection, $sql))
+        {
+           die('Error while executing sql query');
+        }
+    }
+
+    $connection->close();
+
+    //echo $jsonArrayResponse["records"][0]["fields"]["city"] . "\n";
+    //var_dump($jsonArrayResponse);
+?>

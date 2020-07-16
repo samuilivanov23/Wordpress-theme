efc5a5f774a9425dcdf4f7983a606e0b01e1fc6a
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-07-16 09:19:04 -0700

    Adding filtering by two markers positions

--- front-page.php
+++ front-page.php
@@ -5,12 +5,20 @@
     <form action="/wordpress/index.php" method="post">
         <label for="city">City</label><br>
         <input type="text" id="city" name="city"><br>
-        <label for="state">State:</label><br>
+        <label for="state">State</label><br>
         <input type="text" id="state" name="state"><br><br>
+        <label for="state">First point latitude</label><br>
+        <input type="text" id="first_point_latitude" name="first_point_latitude"><br><br>
+        <label for="state">First point longitude</label><br>
+        <input type="text" id="first_point_longitude" name="first_point_longitude"><br><br>
+        <label for="state">Second point latitude</label><br>
+        <input type="text" id="second_point_latitude" name="second_point_latitude"><br><br>
+        <label for="state">Second point longitude</label><br>
+        <input type="text" id="second_point_longitude" name="second_point_longitude"><br><br>
         <input type="submit" value="Submit">
     </form>
 
-    <div id="map">
-    </div>
+    <div id="map"></div>
+    <div id="current">Nothing yet...</div>
 
 <?php get_footer();?>
--- header.php
+++ header.php
@@ -18,33 +18,155 @@
                 die("Connection failed: " . $connection->connect_error);
             }
 
-            echo "<p>city: " . $_POST["city"] . "</p>";
-            echo "<p>state: " . $_POST["state"] . "</p>";
-
-            $city = $_POST["city"];;
+            //taking filter data
+            $city = $_POST["city"];
             $state = $_POST["state"];
+            $first_point_latitude = $_POST["first_point_latitude"];
+            $first_point_longitude = $_POST["first_point_longitude"];
+            $second_point_latitude = $_POST["second_point_latitude"];
+            $second_point_longitude = $_POST["second_point_longitude"];
+
             $sql_query = "";
 
-            if($_POST["city"] == "" && $_POST["state"] == "")
+            if($city == "" && $state == "")
             {
-                echo "<p>i dvete sa null</p>";
-                $sql_query = "select * from locations";
+                if($first_point_latitude == "" && $first_point_longitude == "" && $second_point_latitude == "" & $second_point_longitude == "")
+                {
+                    echo "<p>i dvete sa null</p>";
+                    $sql_query = "select * from locations";
+                }
+                else 
+                {
+                    if($first_point_longitude > $second_point_longitude)
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+
+                    }
+                    else
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+                    }
+                }
             }
             else if($_POST["city"] != "")
             {
-                echo "<p>state e null</p>";
-                $sql_query = "select * from locations where city = '".$city."'";
+                if($first_point_latitude == "" && $first_point_longitude == "" && $second_point_latitude == "" & $second_point_longitude == "")
+                {
+                    echo "<p>state e null</p>";
+                    $sql_query = "select * from locations where city = '".$city."'";
+                }
+                else
+                {
+                    if($first_point_longitude > $second_point_longitude)
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+
+                    }
+                    else
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+                    }
+                }
             }
             else if($_POST["state"] != "")
             {
-                echo "<p>city e null</p>";
-                $sql_query = "select * from locations where state_ = '".$state."'";
+                if($first_point_latitude == "" && $first_point_longitude == "" && $second_point_latitude == "" & $second_point_longitude == "")
+                {
+                    echo "<p>city e null</p>";
+                    $sql_query = "select * from locations where state_ = '".$state."'";
+                }
+                else
+                {
+                    if($first_point_longitude > $second_point_longitude)
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude <= '" . $first_point_longitude . "'" 
+                                                                . "and longitude >= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+
+                    }
+                    else
+                    {
+                        if($first_point_latitude > $second_point_latitude)
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude <= '" . $first_point_latitude . "'"
+                                                                . "and latitude >= '" . $second_point_latitude . "'";
+                        }
+                        else
+                        {
+                            $sql_query = "select * from locations where longitude >= '" . $first_point_longitude . "'" 
+                                                                . "and longitude <= '" . $second_point_longitude . "'"
+                                                                . "and latitude >= '" . $first_point_latitude . "'"
+                                                                . "and latitude <= '" . $second_point_latitude . "'";
+                        }
+                    }
+                }
             }
 
-            echo "<p> $sql_query </p>";
-
-            //$sql_query = "select * from locations";
-            
             $result = $connection->query($sql_query);
             $latitude_array = array();
             $longitude_array = array();
@@ -91,6 +213,25 @@
                 // Add a marker clusterer to manage the markers.
                 var markerCluster = new MarkerClusterer(map, markers,
                     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+                
+                var first_marker;
+
+                for (i = 0; i < markers.length; i++)
+                {
+                    google.maps.event.addListener(markers[i], 'click', function (evt) {
+                        
+                        // document.getElementById('current').innerHTML = '<p>Marker position: Lat: ' + evt.latLng.lat().toFixed(6) + 
+                        //                                       ' Lng: ' + evt.latLng.lng().toFixed(6) + '</p>';
+
+                        first_marker = new google.maps.Marker({
+                            position: new google.maps.LatLng(evt.latLng.lat().toFixed(6), evt.latLng.lng().toFixed(6)),
+                            //my_index = i
+                        });
+                        
+                        document.getElementById('current').innerHTML = '<p>AGAIN...111..Marker position: Lat: ' + first_marker.position.lat() + 
+                                                            ' Lng: ' + first_marker.position.lng() + '</p>';
+                    });
+                }
             }
 
         </script>

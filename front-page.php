237edbee36d7d344ca03ae763357cdc336b41a3a
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-07-19 02:08:21 -0700

    Changing the style of the front page

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php get_header();?>
 
-    <h1> The map with locations</h1>
-    <h3 id="markers_count"></h3>
+    <h1 class="margin_style"> The map with locations</h1>
+    <h3 class="margin_style" id="markers_count"></h3>
 
-    <form action="/wordpress/index.php" method="post">
+    <form class="margin_style" action="/wordpress/index.php" method="post">
         <label for="city">City</label><br>
         <input type="text" id="city" name="city"><br><br>
         <label for="state">State</label><br>
@@ -17,8 +17,8 @@
         <label for="state">Second point latitude</label><br>
         <input type="text" id="second_point_latitude" name="second_point_latitude"><br><br>
         <label for="state">Second point longitude</label><br>
-        <input type="text" id="second_point_longitude" name="second_point_longitude"><br><br>
-        <input type="submit" value="Submit">
+        <input type="text" id="second_point_longitude" name="second_point_longitude"><br>
+        <input class="button button_style" type="submit" value="Submit">
     </form>
 
     <div id="marker_position">No selected marker yet.</div>
